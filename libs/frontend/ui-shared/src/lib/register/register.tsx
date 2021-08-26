dd497686a93e35620a9bfd49659e60b27f2a275d
Johnny5stillalive <jacobbridges3283@gmail.com>
2021-08-25 17:05:44 -0700

    Added VStack to register
    replaced the div with VStack

--- libs/frontend/ui-shared/src/lib/register/register.tsx
+++ libs/frontend/ui-shared/src/lib/register/register.tsx
@@ -1,4 +1,5 @@
 import './register.module.scss';
+import { VStack } from "@chakra-ui/react";
 
 
 /* eslint-disable-next-line */
@@ -6,13 +7,13 @@ export interface RegisterProps {}
 
 export function Register(props: RegisterProps) {
   return (
-    <div>
+    <VStack>
                 <label>Username : </label>   
                  <input type="text" placeholder="Enter Email" name="email" required />  
                  <label>Password : </label>   
                  <input type="password" placeholder="Enter Password" name="password" required />  
                  <a href="/login-page"><button type="submit">Register</button> </a>
-        </div>
+        </VStack>
   );
 }
 
